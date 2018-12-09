193c09dea48b789986b9813a6f43ee3f4130ba0a
Henry Allsuch <henry@agilepixel.io>
2018-12-09 20:59:02 +0000

    • arrays

--- Swift4.2/Arrays.playground/Contents.swift
+++ Swift4.2/Arrays.playground/Contents.swift
@@ -20,3 +20,35 @@ for city in cities {
 for (index, city) in cities.enumerated() {
     print("\(index): \(city)")
 }
+
+let tree : Character = "🎄"
+var sixTrees = Array(repeating: tree, count: 6)
+
+
+//access
+
+cities[0]
+cities[0...2]
+
+let firstCity = cities[cities.startIndex]
+
+let lastCity = cities[cities.index(before: cities.endIndex)]
+
+let penultimateCity = cities[cities.index(cities.endIndex, offsetBy: -2)]
+
+let invalidIndex = moreCities.index(moreCities.endIndex, offsetBy: -3)
+moreCities.indices.contains(invalidIndex)
+
+
+//methods
+
+cities.count
+cities.isEmpty
+
+let initials = cities.map { $0.first! }
+print(initials)
+
+let allLetterCount = cities.reduce(0,{ $0 + $1.count })
+let allLetterCountVerbose = cities.reduce(0,{ total, city in total + city.count })
+print(allLetterCount,allLetterCountVerbose)
+
