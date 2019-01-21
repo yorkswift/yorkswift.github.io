3c59091a869ae76fa8ac9e6cb5e18245414956d6
Henry Allsuch <henry@agilepixel.io>
2019-01-21 23:03:50 +0000

    • codable

--- Swift4.2/Codable.playground/Contents.swift
+++ Swift4.2/Codable.playground/Contents.swift
@@ -1,36 +1,180 @@
 import Foundation
 
-/// https://restcountries.eu/rest/v2/region/europe
-
 let json = """
-[{"name":"United Kingdom of Great Britain and Northern Ireland","topLevelDomain":[".uk"],"alpha2Code":"GB","alpha3Code":"GBR","callingCodes":["44"],"capital":"London","altSpellings":["GB","UK","Great Britain"],"region":"Europe","subregion":"Northern Europe","population":65110000,"latlng":[54.0,-2.0],"demonym":"British","area":242900.0,"gini":34.0,"timezones":["UTC-08:00","UTC-05:00","UTC-04:00","UTC-03:00","UTC-02:00","UTC","UTC+01:00","UTC+02:00","UTC+06:00"],"borders":["IRL"],"nativeName":"United Kingdom","numericCode":"826","currencies":[{"code":"GBP","name":"British pound","symbol":"£"}],"languages":[{"iso639_1":"en","iso639_2":"eng","name":"English","nativeName":"English"}],"translations":{"de":"Vereinigtes Königreich","es":"Reino Unido","fr":"Royaume-Uni","ja":"イギリス","it":"Regno Unito","br":"Reino Unido","pt":"Reino Unido","nl":"Verenigd Koninkrijk","hr":"Ujedinjeno Kraljevstvo","fa":"بریتانیای کبیر و ایرلند شمالی"},"flag":"https://restcountries.eu/data/gbr.svg","regionalBlocs":[{"acronym":"EU","name":"European Union","otherAcronyms":[],"otherNames":[]}],"cioc":"GBR"}]
+[
+	{
+		"name": "United Kingdom of Great Britain and Northern Ireland",
+		"alpha2Code": "GB",
+		"capital": "London",
+		"region": "Europe",
+		"population": 65110000,
+		"latlng": [
+			54.0,
+			-2.0
+		],
+		"demonym": "British",
+		"nativeName": "United Kingdom",
+		"flag": "https://restcountries.eu/data/gbr.svg",
+		"regionalBlocs": [
+			{
+				"acronym": "EU",
+				"name": "European Union ??"
+			}
+		]
+	},
+    {
+        "name": "Italy",
+        "alpha2Code": "IT",
+        "capital": "Rome",
+        "region": "Europe",
+        "population": 60665551,
+        "latlng": [
+            42.83333333,
+            12.83333333
+        ],
+        "demonym": "Italian",
+        "nativeName": "Italia",
+        "regionalBlocs": [
+            {
+                "acronym": "EU",
+                "name": "European Union"
+            }
+        ],
+    },
+]
 """
 
-//
+let countryRawData = Data(json.utf8)
+let decoder = JSONDecoder()
 
-struct Country: Codable {
+/**
+ 
+Basic Decoding
+
+**/
+
+struct SimpleCountry: Codable {
     var name: String
     var capital: String
 }
 
-let data = Data(json.utf8)
-let decoder = JSONDecoder()
+typealias SimpleCountries = [SimpleCountry]
 
 do {
-    let europe = try decoder.decode([Country].self, from: data)
     
-    print(europe.first)
+    let europe = try decoder.decode(SimpleCountries.self, from: countryRawData)
+    
+    for county in europe {
+        print("\(county.capital) is the capital city of \(county.name)")
+    }
+    
+} catch {
+    print("\(error)")
+}
+
+/*
+Nested Decoding with custom keys
+*/
+
+typealias Countries = [Country]
+
+struct Country: Codable {
+    
+    var regions : [Region]
+    
+    enum CodingKeys: String, CodingKey {
+        case regions = "regionalBlocs"
+    }
+}
+
+struct Region : Codable {
+    var acronym : String
+    var name : String
+}
+
+do {
+    
+    let europe = try decoder.decode(Countries.self, from: countryRawData)
+    
+    for county in europe {
+
+        if let region = county.regions.first?.name {
+            print(region)
+        }
+    }
     
 } catch {
     print("\(error)")
 }
 
 
+/*
+ Custom Decoding
+*/
 
-//do {
-//    let encoder = JSONEncoder()
-//    let data = try encoder.encode(user)
-//
-//} catch {
-//    print("\(error)")
-//}
+typealias GeoCountries = [GeoCountry]
+
+struct GeoCountry: Codable {
+    
+    var code : String
+    var location: Coordinate
+    
+    enum CodingKeys: String, CodingKey {
+        case location = "latlng"
+        case code = "alpha2Code"
+    }
+}
+
+struct Coordinate: Codable {
+    
+    var latitude: Float?
+    var longitude: Float?
+    
+    static let latitudeCoordinateIndex = 1
+    static let longitudeCoordinateIndex = 2
+    
+    public init(from decoder: Decoder) throws {
+        
+        var coordinates = try decoder.unkeyedContainer()
+        
+        while (!coordinates.isAtEnd) {
+            
+            let coordinate = try coordinates.decode(Float.self)
+            
+            if(coordinates.currentIndex == Coordinate.latitudeCoordinateIndex){
+                latitude = coordinate
+            } else if(coordinates.currentIndex == Coordinate.longitudeCoordinateIndex){
+                longitude = coordinate
+            }
+        }
+    
+    }
+}
+
+do {
+    
+    let europe = try decoder.decode(GeoCountries.self, from: countryRawData)
+    
+    enum DecodingError: Error {
+        case message(String)
+    }
+    
+    for county in europe {
+        
+        guard let long = county.location.longitude else { throw DecodingError.message("Missing longitude")  }
+        guard let lat = county.location.latitude else { throw DecodingError.message("Missing latitude")  }
+        
+        print("\(county.code) = (\(long),\(lat))")
+
+    }
+    
+} catch {
+    print("\(error)")
+}
+
+/**
+ 
+ Source:
+ https://restcountries.eu/rest/v2/region/europe
+ 
+ **/
